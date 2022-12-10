30492c6fb1ddd7659fdaacef774b8872e0f3c625
Juan Lopes <me@juanlopes.net>
2022-12-10 17:36:46 -0300

    Day 10

---
+++ day10.py
@@ -0,0 +1,14 @@
+import sys
+def execute(program):
+    X = 1
+    for line in program:
+        yield X
+        if line[0] == 'addx':
+            yield X
+            X += int(line[1])
+
+S = list(execute(line.split() for line in sys.stdin.read().split('\n')))
+print(sum(S[i-1]*i for i in [20, 60, 100, 140, 180, 220]))
+
+for i in range(6):
+    print(''.join('.#'[abs(S[i*40+j] - j) <= 1] for j in range(40)))
