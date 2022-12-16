58aa28b5d315f4083e22b554d9d48142a7347d33
Juan Lopes <me@juanlopes.net>
2022-12-16 15:58:01 -0300

    Day 16

---
+++ day16.py
@@ -0,0 +1,24 @@
+import sys, re
+lines = [re.split('[\\s=;,]+', x) for x in sys.stdin.read().splitlines()]
+G = {x[1]: set(x[10:]) for x in lines}
+F = {x[1]: int(x[5]) for x in lines if int(x[5]) != 0}
+I = {x: 1<<i for i, x in enumerate(F)}
+T = {x: {y: 1 if y in G[x] else float('+inf') for y in G} for x in G}
+for k in T:
+    for i in T:
+        for j in T:
+            T[i][j] = min(T[i][j], T[i][k]+T[k][j])
+
+def visit(v, budget, state, value, answer):
+    answer[state] = max(answer.get(state, 0), value)
+    for u in F:
+        newbudget = budget - T[v][u] - 1
+        if I[u] & state or newbudget < 0: continue
+        visit(u, newbudget, state | I[u], value + newbudget * F[u], answer)
+    return answer    
+
+total1 = max(visit('AA', 30, 0, 0, {}).values())
+visited2 = visit('AA', 26, 0, 0, {})
+total2 = max(v1+v2 for k1, v1 in visited2.items() 
+                   for k2, v2 in visited2.items() if not k1 & k2)
+print(total1, total2)
