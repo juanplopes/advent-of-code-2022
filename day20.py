3b19ba7b8233a7b986cb1832e7929d8ec89fe01d
Juan Lopes <me@juanlopes.net>
2022-12-20 11:28:12 -0300

    Improve day 20 performance

--- day20.py
+++ day20.py
@@ -1,18 +1,14 @@
 import sys
-T = [[int(x), None] for x in sys.stdin.read().splitlines()]
-for i in range(len(T)-1, 0, -1):
-    T[i-1][1] = T[i]
+T = [int(x) for x in sys.stdin.read().splitlines()]
 
-def solve(start, key, times):
-    Q = list(T)
+def solve(key, times):
+    I = list(range(len(T)))
     for _ in range(times):
-        cur = start
-        while cur != None:
-            idx = Q.index(cur)
-            Q.pop(idx)
-            Q.insert((idx + cur[0] * key) % len(Q), cur)
-            cur = cur[1]
-    zero = [x for x, _ in Q].index(0)
-    return sum(Q[(zero+i) % len(Q)][0] * key for i in (1000, 2000, 3000))
+        for i in range(len(T)):
+            idx = I.index(i)
+            I.pop(idx)
+            I.insert((idx + T[i] * key) % len(I), i)
+    zero = I.index(T.index(0))
+    return sum(T[I[(zero+i) % len(I)]] * key for i in (1000, 2000, 3000))
 
-print(solve(T[0], 1, 1), solve(T[0], 811589153, 10))
+print(solve(1, 1), solve(811589153, 10))
