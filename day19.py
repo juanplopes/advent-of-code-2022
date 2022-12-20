059f577a0a1653be1b9684d6dcc6ee0432c6f725
Juan Lopes <me@juanlopes.net>
2022-12-20 18:06:25 -0300

    Improving day 19

--- day19.py
+++ day19.py
@@ -1,36 +1,33 @@
-import sys, re, functools, heapq
+import sys, re, functools
 
-def res(req, cur, prod):
-    return max((req - cur + prod - 1) // prod, 0) + 1
+def timeto(req, cur, prod):
+    return max(((req - cur + prod - 1) // prod), 0) + 1
 
 def T(blueprint, mins):
     _, co, cc, cb1, cb2, cg1, cg2 = blueprint
-    mo = max(cc, cb1, cg1)
-    Q, V, maxx = [], {}, 0
-    def p(g, t, o, c, b, ro, rc, rb):
+    mo, maxx = max(cc, cb1, cg1), 0
+    def dfs(g, t, o, c, b, ro, rc, rb):
         nonlocal maxx
-        k = (t, o, c, b, ro, rc, rb)
-        if t < 0 or -g+t*t-t < maxx or V.get(k, -1) >= -g: return
-        maxx = max(maxx, -g)
-        V[k] = -g
-        heapq.heappush(Q, (g, t, o, c, b, ro, rc, rb))
+        maxg = g+t*t-t
+        if t <= 0 or maxg <= maxx: return
+        maxx = max(maxx, g)
+        if rb: # create geode robot?
+            dt = max(timeto(cg1, o, ro), timeto(cg2, b, rb))
+            dfs(g + max(t-dt, 0), t-dt, o+dt*ro-cg1, c+dt*rc, b+dt*rb-cg2, ro, rc, rb)
+        if maxg <= maxx: return
+        if rc and b+(rb*t) < cg2*t: #create obsidian robot?
+            dt = max(timeto(cb1, o, ro), timeto(cb2, c, rc))
+            dfs(g, t-dt, o+dt*ro-cb1, c+dt*rc-cb2, b+dt*rb, ro, rc, rb+1)
+        if maxg <= maxx: return
+        if c+(rc*t) < cb2*t: # create clay robot?
+            dt = timeto(cc, o, ro)
+            dfs(g, t-dt, o+dt*ro-cc, c+dt*rc, b+dt*rb, ro, rc+1, rb)
+        if maxg <= maxx: return
+        if o+(ro*t) < mo*t: # create ore robot?
+            dt = timeto(co, o, ro)
+            dfs(g, t-dt, o+dt*ro-co, c+dt*rc, b+dt*rb, ro+1, rc, rb)
 
-    p(0, mins, 0, 0, 0, 1, 0, 0)
-    while Q:
-        g, t, o, c, b, ro, rc, rb = heapq.heappop(Q)
-        if -g+t*t-t < maxx: continue
-        if rb:
-            dt = max(res(cg1, o, ro), res(cg2, b, rb))
-            p(g - max(t-dt, 0), t-dt, o+dt*ro-cg1, c+dt*rc, b+dt*rb-cg2, ro, rc, rb)
-        if rc and rb < cg2 and b+(rb*t) < cg2*t:
-            dt = max(res(cb1, o, ro), res(cb2, c, rc))
-            p(g, t-dt, o+dt*ro-cb1, c+dt*rc-cb2, b+dt*rb, ro, rc, rb+1)
-        if rc < cb2 and c+(rc*t) < cb2*t:
-            dt = res(cc, o, ro)
-            p(g, t-dt, o+dt*ro-cc, c+dt*rc, b+dt*rb, ro, rc+1, rb)
-        if ro < mo and o+(ro*t) < mo*t:
-            dt = res(co, o, ro)
-            p(g, t-dt, o+dt*ro-co, c+dt*rc, b+dt*rb, ro+1, rc, rb)
+    dfs(0, mins, 0, 0, 0, 1, 0, 0)
     return maxx    
 
 lines = [tuple(map(int, re.findall('\\d+', x))) for x in sys.stdin.read().splitlines()]
