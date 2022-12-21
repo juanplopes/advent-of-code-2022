8be01461bb6077bb8010627b30c602aaa19e0332
Juan Lopes <me@juanlopes.net>
2022-12-21 12:02:46 -0300

    Day 21

--- day21.py
+++ day21.py
@@ -2,7 +2,7 @@ import sys, re
 OP = {'+': lambda a, b: a+b, '-': lambda a, b: a-b, 
       '*': lambda a, b: a*b, '/': lambda a, b: a/b}
 lines = [re.split('[\\s:]+', x) for x in sys.stdin.read().splitlines()]
-T = {x[0] : x[1:] for x in lines}
+T = {x[0]: x[1:] for x in lines}
 def evaluate(T, node):
     if len(node) == 1: return complex(node[0])
     return OP[node[1]](evaluate(T, T[node[0]]), evaluate(T, T[node[2]]))
