d6fb21e51940820ed4d4156a0bc4f738fc3e3cea
Juan Lopes <me@juanlopes.net>
2022-12-21 12:02:30 -0300

    Day 21

--- day21.py
+++ day21.py
@@ -7,6 +7,6 @@ def evaluate(T, node):
     if len(node) == 1: return complex(node[0])
     return OP[node[1]](evaluate(T, T[node[0]]), evaluate(T, T[node[2]]))
 
-Q = dict(T) | {'humn': [1j]}
+Q = T | {'humn': [1j]}
 r2 = evaluate(Q, Q[Q['root'][0]]) - evaluate(Q, Q[Q['root'][2]])
 print(int(evaluate(T, T['root']).real), round(r2.real/-r2.imag))
