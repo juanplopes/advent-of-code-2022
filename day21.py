621c686578764a804387c2cad38535dac689f885
Juan Lopes <me@juanlopes.net>
2022-12-21 11:53:02 -0300

    Day 21

--- day21.py
+++ day21.py
@@ -1,28 +1,14 @@
-import sys, re
-OP = {'+': int.__add__, '-': int.__sub__, '*': int.__mul__, '/': int.__floordiv__}
+import sys, re, math
+OP = {'+': lambda a, b: a+b, '-': lambda a, b: a-b, 
+      '*': lambda a, b: a*b, '/': lambda a, b: a/b}
 lines = [re.split('[\\s:]+', x) for x in sys.stdin.read().splitlines()]
 T = {x[0] : x[1:] for x in lines}
 def evaluate(T, monkey):
     node = T[monkey]
-    if node == ['x']: return node
-    if len(node) == 1: return int(node[0])
-    a, b = evaluate(T, node[0]), evaluate(T, node[2])
-    if not isinstance(a, int) or not isinstance(b, int): 
-        return [a, node[1], b]
-    return OP[node[1]](a, b)
+    if len(node) == 1: return complex(node[0])
+    return OP[node[1]](evaluate(T, node[0]), evaluate(T, node[2]))
 
-def solve2():
-    Q = dict(T)
-    Q['root'] = [Q['root'][0], '=', Q['root'][2]]
-    Q['humn'] = ['x']
-    expr, _,c  = evaluate(Q, 'root')
-    if isinstance(expr, int): c, expr = expr, c
-    while expr != ['x']:
-        a, op, b = expr
-        if isinstance(a, int):
-            expr, c = {'+': (b, c-a), '-': (b, a-c), '*': (b, c//a), '/': (b, a//c)}[op]
-        if isinstance(b, int):
-            expr, c = {'+': (a, c-b), '-': (a, b+c), '*': (a, c//b), '/': (a, b*c)}[op]
-    return c
-
-print(evaluate(T, 'root'), solve2())
+Q = dict(T)
+Q['humn'] = [1j]
+r2 = evaluate(Q, Q['root'][0]) - evaluate(Q, Q['root'][2])     
+print(int(evaluate(T, 'root').real), round(r2.real/-r2.imag))
