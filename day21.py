e2b9383502ab1421bf4192a15548f908037d481d
Juan Lopes <me@juanlopes.net>
2022-12-21 12:01:52 -0300

    Day 21

--- day21.py
+++ day21.py
@@ -3,12 +3,10 @@ OP = {'+': lambda a, b: a+b, '-': lambda a, b: a-b,
       '*': lambda a, b: a*b, '/': lambda a, b: a/b}
 lines = [re.split('[\\s:]+', x) for x in sys.stdin.read().splitlines()]
 T = {x[0] : x[1:] for x in lines}
-def evaluate(T, monkey):
-    node = T[monkey]
+def evaluate(T, node):
     if len(node) == 1: return complex(node[0])
-    return OP[node[1]](evaluate(T, node[0]), evaluate(T, node[2]))
+    return OP[node[1]](evaluate(T, T[node[0]]), evaluate(T, T[node[2]]))
 
-Q = dict(T)
-Q['humn'] = [1j]
-r2 = evaluate(Q, Q['root'][0]) - evaluate(Q, Q['root'][2])     
-print(int(evaluate(T, 'root').real), round(r2.real/-r2.imag))
+Q = dict(T) | {'humn': [1j]}
+r2 = evaluate(Q, Q[Q['root'][0]]) - evaluate(Q, Q[Q['root'][2]])
+print(int(evaluate(T, T['root']).real), round(r2.real/-r2.imag))
