63a4fe42d0bf7d80e409eae472d932bb7f1c2716
Juan Lopes <me@juanlopes.net>
2022-12-21 12:36:04 -0300

    Day 21

--- day21.py
+++ day21.py
@@ -1,9 +1,8 @@
 import sys, re, operator
 OP = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
 T = {x[0]: x[1:] for x in (re.split('[\\s:]+', x) for x in sys.stdin.read().splitlines())}
-def evaluate(T, x):
-    if len(x) == 1: return complex(x[0])
-    return OP[x[1]](evaluate(T, T[x[0]]), evaluate(T, T[x[2]]))
+def ev(T, x):
+    return complex(x[0]) if len(x) == 1 else OP[x[1]](ev(T, T[x[0]]), ev(T, T[x[2]]))
 
-r2 = evaluate(T | {'humn': [1j]}, [T['root'][0], '-', T['root'][2]])
-print(int(evaluate(T, T['root']).real), round(r2.real/-r2.imag))
+r2 = ev(T | {'humn': [1j]}, [T['root'][0], '-', T['root'][2]])
+print(int(ev(T, T['root']).real), round(r2.real/-r2.imag))
